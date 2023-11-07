68772f8644d22bd33d4cab42c7e718d306777704
Udoy <68466005+partho0@users.noreply.github.com>
2023-11-07 09:00:11 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# phone_number_detail
+ 
---
+++ number.py
@@ -0,0 +1,11 @@
+import phonenumbers
+from phonenumbers import timezone,geocoder,carrier
+number = input("enter your number with country code: ")
+phone = phonenumbers.parse(number)
+time = timezone.time_zones_for_number(phone)
+carr = carrier.name_for_number(phone, "en")
+reg = geocoder.description_for_number(phone, "en")
+print(phone)
+print(time)
+print(carr)
+print(reg)
